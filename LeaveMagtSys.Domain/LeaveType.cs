953f7f46eae9324f258a4470c48d85cbd6e8ff2b
emekat <emekatorti@gmail.com>
2023-03-29 11:49:10 -0400

    Add project files.

---
+++ LeaveMagtSys.Domain/LeaveType.cs
@@ -0,0 +1,16 @@
+﻿using LeaveMagtSys.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveMagtSys.Domain
+{
+    public class LeaveType: BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public int DefaultDays { get; set; }
+    }
+}
