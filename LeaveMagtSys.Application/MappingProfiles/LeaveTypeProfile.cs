c1a1aeb10b1027357fb0810ded82f92cfd174457
emekat <emekatorti@gmail.com>
2023-03-29 12:38:26 -0400

    Added AutoMapper settings

---
+++ LeaveMagtSys.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LeaveMagtSys.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using LeaveMagtSys.Domain;
+
+namespace LeaveMagtSys.Application.MappingProfiles
+{
+    public class LeaveTypeProfile : Profile
+    {
+        public LeaveTypeProfile()
+        {
+            CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
+        }
+    }
+}
